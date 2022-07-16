738728dc5c29ce3836f727958f9c547e65df0599
Rawnak-Siddique <g0urney0fth0u5andm11e5@gmail.com>
2022-07-16 13:15:20 +0600

    updated 2

--- src/pages/Leads/Leads.js
+++ src/pages/Leads/Leads.js
@@ -1,11 +1,9 @@
 import MaterialTable, { MTableToolbar } from 'material-table';
 import React, { useState } from 'react'
-import { OptionsButton } from '../Inventory/styles';
 import { LeadsBody, LeadsButton, LeadsHeader } from './styles'
 import { useNavigate } from "react-router-dom";
 
 const Leads = () => {
-    const [filtering, setFiltering] = useState(false);
     const [columns, setColumns] = useState([
         {  title: 'First Name', field: 'first-name' },
         {  title: 'Last Name', field: 'last-name' },
@@ -42,44 +40,9 @@ const Leads = () => {
                     columns={columns}
                     data={data}
                     options={{
-                        filtering: filtering,
                         search: true,
-                        selection: true,
                     }}
-                    components={{
-                        Toolbar: props => (
-                        <div>
-                            <MTableToolbar {...props} />
-                            <div>
-                            <OptionsButton onClick={() => {setFiltering(!filtering)}} >Filtering</OptionsButton>
-                            </div>
-                        </div>
-                        )
-                    }}
-                    editable={{  
-                        onRowUpdate: (newData, oldData) =>
-                            new Promise((resolve, reject) => {
-                            setTimeout(() => {
-                            const dataUpdate = [...data];
-                            const index = oldData.tableData.id;
-                            dataUpdate[index] = newData;
-                            setData([...dataUpdate]);
-
-                            resolve();
-                            }, 1000)
-                        }),
-                        onRowDelete: oldData =>
-                        new Promise((resolve, reject) => {
-                            setTimeout(() => {
-                            const dataDelete = [...data];
-                            const index = oldData.tableData.id;
-                            dataDelete.splice(index, 1);
-                            setData([...dataDelete]);
-                            
-                            resolve()
-                            }, 1000)
-                        }),
-                    }}
+                    
                 ></MaterialTable>
         </LeadsBody>
     )
--- src/pages/Vendors/Vendors.js
+++ src/pages/Vendors/Vendors.js
@@ -1,11 +1,9 @@
 import MaterialTable, { MTableToolbar } from 'material-table'
 import React, { useState } from 'react'
-import { OptionsButton } from '../Inventory/styles'
 import { VendorsBody, VendorsButton, VendorsHeader } from './styles'
 import { useNavigate } from "react-router-dom";
 
 const Vendors = () => {
-    const [filtering, setFiltering] = useState(false);
     const [columns, setColumns] = useState([
         { title: 'Company Name', field: 'company-name' },
         { title: 'First Name', field: 'first-name' },
@@ -40,43 +38,7 @@ const Vendors = () => {
                     columns={columns}
                     data={data}
                     options={{
-                        filtering: filtering,
-                        search: true,
-                        selection: true,
-                    }}
-                    components={{
-                        Toolbar: props => (
-                        <div>
-                            <MTableToolbar {...props} />
-                            <div>
-                            <OptionsButton onClick={() => {setFiltering(!filtering)}} >Filtering</OptionsButton>
-                            </div>
-                        </div>
-                        )
-                    }}
-                    editable={{  
-                        onRowUpdate: (newData, oldData) =>
-                            new Promise((resolve, reject) => {
-                            setTimeout(() => {
-                            const dataUpdate = [...data];
-                            const index = oldData.tableData.id;
-                            dataUpdate[index] = newData;
-                            setData([...dataUpdate]);
-
-                            resolve();
-                            }, 1000)
-                        }),
-                        onRowDelete: oldData =>
-                        new Promise((resolve, reject) => {
-                            setTimeout(() => {
-                            const dataDelete = [...data];
-                            const index = oldData.tableData.id;
-                            dataDelete.splice(index, 1);
-                            setData([...dataDelete]);
-                            
-                            resolve()
-                            }, 1000)
-                        }),
+                        search: true,  
                     }}
                 ></MaterialTable>
         </VendorsBody>
